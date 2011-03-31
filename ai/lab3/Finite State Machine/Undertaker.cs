36e147d93488b2289c38da1e0fecb1c1ca8bcd1e
unknown <j0n@.(none)>
2011-03-31 23:19:59 +0100

    Added lab3 skel

---
+++ ai/lab3/Finite State Machine/Undertaker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FiniteStateMachine
+{
+    class Undertaker : Agent
+    {
+        private StateMachine<Undertaker> stateMachine;
+        public StateMachine<Undertaker> StateMachine
+        {
+            get { return stateMachine; }
+            set { stateMachine = value; }
+        }
+
+        private DeadBody currentBody;
+        public DeadBody CurrentBody
+        {
+            get { return currentBody; }
+            set { currentBody = value; }
+        }
+
+        public Undertaker() : base()
+        {
+            stateMachine = new StateMachine<Undertaker>(this);
+            stateMachine.CurrentState = new Hover();
+            stateMachine.GlobalState = new UndertakerGlobalState();
+            location = Location.undertakers;
+            agentType = AgentType.undertaker;
+        }
+
+        public override void Update()
+        {
+            stateMachine.Update();
+        }
+
+        public override bool HandleMessage(Telegram telegram)
+        {
+            return stateMachine.HandleMessage(telegram);
+        }
+    }
+}
---
+++ ai/lab3/Finite State Machine/model/GameModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FiniteStateMachine.map;
+
+namespace FiniteStateMachine.model
+{
+    class GameModel
+    {
+        List<Square> m_Squares;
+
+        public List<Square> Squares
+        {
+            get { return m_Squares; }
+            set { m_Squares = value; }
+        }
+
+        public GameModel()
+        {
+        }
+
+
+    }
+}
